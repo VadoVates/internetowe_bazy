e200307cbb7ff13d4a14c234704f5dc897c7dcad
VadoVates <marekgor@gmail.com>
2024-11-16 21:58:56 +0100

    init+

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         $pdo = new PDO('mysql:host=' . $host . ';dbname=' . $dbname . ';port=' . $port, $username, $password);
         echo 'Połączenie nawiązane! <br />';
     } catch (PDOException $e) {
-        / Wybrałem zapis błędu do pliku error_log.log. Trzeba przypilnować aby ten plik istniał
+        /* Wybrałem zapis błędu do pliku error_log.log. Trzeba przypilnować aby ten plik istniał
          oraz aby była możliwość zapisu do niego (uprawnienia) */
         echo 'Połączenie z bazą danych nie zostało utworzone. <br />';
         $errormsg= "[" . date('Y-m-d H:i:s') . "] " . (string)$e . PHP_EOL;
