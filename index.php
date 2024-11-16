25919182ce72a008e1dbfd3eb69a123dcd47ae81
VadoVates <marekgor@gmail.com>
2024-11-16 21:57:54 +0100

    init

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    try {
+        $pdo = new PDO('mysql:host=' . $host . ';dbname=' . $dbname . ';port=' . $port, $username, $password);
+        echo 'Połączenie nawiązane! <br />';
+    } catch (PDOException $e) {
+        / Wybrałem zapis błędu do pliku error_log.log. Trzeba przypilnować aby ten plik istniał
+         oraz aby była możliwość zapisu do niego (uprawnienia) */
+        echo 'Połączenie z bazą danych nie zostało utworzone. <br />';
+        $errormsg= "[" . date('Y-m-d H:i:s') . "] " . (string)$e . PHP_EOL;
+        error_log($errormsg, 3, 'error_log.log');
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Hello World - Internetowe Bazy Danych</title>
+    </head>
+    <body>
+        <?php echo '<p>Hello World!</p>'; ?>
+    </body>
+</html>
---
+++ test.sql
@@ -0,0 +1,109 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Lis 15, 2024 at 02:36 PM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `audit_subscribers`
+--
+
+CREATE TABLE `audit_subscribers` (
+  `id` int(11) NOT NULL,
+  `subscriber_name` varchar(255) NOT NULL,
+  `action_performed` text NOT NULL,
+  `date_added` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `subscribers`
+--
+
+CREATE TABLE `subscribers` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Wyzwalacze `subscribers`
+--
+DELIMITER $$
+CREATE TRIGGER `after_subscriber_delete` AFTER DELETE ON `subscribers` FOR EACH ROW BEGIN
+	INSERT INTO audit_subscribers (subscriber_name, action_performed)
+	VALUES (OLD.fname, 'Deletetd a subscriber');
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `after_subscriber_edit` AFTER UPDATE ON `subscribers` FOR EACH ROW BEGIN
+	INSERT INTO audit_subscribers (subscriber_name, action_performed)
+    VALUES (NEW.fname, 'Updated a subscriber');
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `before_subscriber_insert` BEFORE INSERT ON `subscribers` FOR EACH ROW BEGIN
+	INSERT INTO audit_subscribers (subscriber_name, action_performed)
+    VALUES (NEW.fname, 'Insert a new subscriber');
+END
+$$
+DELIMITER ;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `audit_subscribers`
+--
+ALTER TABLE `audit_subscribers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeksy dla tabeli `subscribers`
+--
+ALTER TABLE `subscribers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `audit_subscribers`
+--
+ALTER TABLE `audit_subscribers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `subscribers`
+--
+ALTER TABLE `subscribers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
