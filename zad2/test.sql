4a98b3dff42b27efbe4a94a1d74db5f84ca898a4
VadoVates <marekgor@gmail.com>
2024-11-24 21:16:47 +0100

    idea to improve

--- zad2/test.sql
+++ zad2/test.sql
@@ -11,7 +11,7 @@ CREATE USER 'int_baz'@'localhost' IDENTIFIED BY '1nt3rn3t0w3_b4zy';
 
 CREATE TABLE `audit_subscribers` (
   `audit_id` INT NOT NULL,
-  `id` INT NOT NULL,
+  `user_id` INT NOT NULL,
   `subscriber_name` VARCHAR(255) NOT NULL,
   `action_performed` TEXT NOT NULL,
   `date_added` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
@@ -50,6 +50,15 @@ END
 $$
 DELIMITER ;
 
+-- wrzucenie ID użytkownika w after subscriber insert
+DELIMITER $$
+CREATE TRIGGER `after_subscriber_insert` AFTER DELETE ON `subscribers` FOR EACH ROW BEGIN
+	INSERT INTO audit_subscribers (id, subscriber_name, action_performed)
+	VALUES (OLD.id, OLD.fname, 'Deleted a subscriber');
+END
+$$
+DELIMITER ;
+
 ALTER TABLE `audit_subscribers`
   ADD PRIMARY KEY (`audit_id`);
 
@@ -60,7 +69,7 @@ ALTER TABLE `subscribers`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `audit_subscribers`
-  MODIFY `audit_id` int(11) NOT NULL AUTO INCREMENT;
+  MODIFY `audit_id` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `audit_subscribers`
-  ADD FOREIGN KEY (`id`) REFERENCES `subscribers` (`id`);
+  ADD FOREIGN KEY (`user_id`) REFERENCES `subscribers` (`id`);
