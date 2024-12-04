7a1b73b71931f2a921e2244f6c2bc2777dbd6863
VadoVates <marekgor@gmail.com>
2024-12-04 22:32:59 +0100

    Added new subpage with possibility of displaying views

--- zad3/test.sql
+++ zad3/test.sql
@@ -4,14 +4,19 @@
 -- rok akademicki 2024/2025
 -- semestr V
 
+-- stworzenie bazy danych
 CREATE DATABASE test;
+-- użycie stworzonej bazy
 USE test;
 
+-- stworzenie użytkownika i przypisanie hasła
 CREATE USER 'int_baz'@'localhost' IDENTIFIED BY '1nt3rn3t0w3_b4zy';
 
+
+-- stworzenie tablic z odpowiednimi polami
 CREATE TABLE `audit_subscribers` (
   `id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
+  `user_id` int (11) DEFAULT NULL,
   `subscriber_name` varchar(255) NOT NULL,
   `action_performed` text NOT NULL,
   `date_added` timestamp NOT NULL DEFAULT current_timestamp()
@@ -23,9 +28,14 @@ CREATE TABLE `subscribers` (
   `email` varchar(255) NOT NULL
 );
 
+-- przyznanie uprawnień użytkownikowi do tej bazy danych
 GRANT INSERT, UPDATE, DELETE, SELECT, TRIGGER ON test.* TO 'int_baz'@'localhost';
 FLUSH PRIVILEGES;
 
+GRANT INSERT, UPDATE, DELETE, SELECT, TRIGGER ON test.* TO 'int_baz'@'localhost';
+FLUSH PRIVILEGES;
+
+-- trigger `after_subscriber_delete` dodający informację o usunięciu użytkownika
 DELIMITER $$
 CREATE TRIGGER `after_subscriber_delete` AFTER DELETE ON `subscribers` FOR EACH ROW BEGIN
 	INSERT INTO audit_subscribers (user_id, subscriber_name, action_performed)
@@ -34,6 +44,7 @@ END
 $$
 DELIMITER ;
 
+-- trigger `after_subscriber_edit` rejestrujący akcję edycji
 DELIMITER $$
 CREATE TRIGGER `after_subscriber_edit` AFTER UPDATE ON `subscribers` FOR EACH ROW BEGIN
 	INSERT INTO audit_subscribers (user_id, subscriber_name, action_performed)
@@ -42,6 +53,8 @@ END
 $$
 DELIMITER ;
 
+-- trigger `before_subscriber_insert` rejestrujący przed dodaniem, informację o dodaniu nowego usera
+-- jest napisany w dość skomplikowany sposób przez dziwne wymogi zadania trzeciego
 DELIMITER $$
 CREATE TRIGGER `before_subscriber_insert` BEFORE INSERT ON `subscribers` FOR EACH ROW BEGIN
   DECLARE last_id INT;
@@ -54,53 +67,68 @@ END
 $$
 DELIMITER ;
 
+-- dodanie klucza głównego tabeli `audit_subscribers`
 ALTER TABLE `audit_subscribers`
   ADD PRIMARY KEY (`id`);
 
+-- dodanie klucza głównego tabeli `subscribers`
 ALTER TABLE `subscribers`
   ADD PRIMARY KEY (`id`);
 
+-- zmiana parametrów klucza głównego - nie może być pusty i jest automatycznie inkrementowany
 ALTER TABLE `subscribers`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+-- liczenie zaczynam od 1
 ALTER TABLE `subscribers`
   AUTO_INCREMENT = 1;
 
+-- zmiana parametrów klucza głównego - nie może być pusty i jest automatycznie inkrementowany
 ALTER TABLE `audit_subscribers`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
+-- liczenie zaczynam od 1
 ALTER TABLE `audit_subscribers`
   AUTO_INCREMENT = 1;
 
 -- pkt. 1 -> widok wyświetlający nazwę użytkowników oraz datę ich dodania
 CREATE VIEW user_creation_view AS
 SELECT
+  user_id,
   subscriber_name,
   date_added AS creation_date
 FROM
   audit_subscribers
 WHERE
-  action_performed = 'Insert a new subscriber';
+  action_performed = 'Insert a new subscriber'
+ORDER BY
+  user_id;
 
 -- pkt. 2 -> widok wyświetlający nazwę użytkowników oraz datę ich usunięcia
 CREATE VIEW user_deletion_view AS
 SELECT
+  user_id,
   subscriber_name,
   date_added AS deletion_date
 FROM
   audit_subscribers
 WHERE
-  action_performed = 'Deleted a subscriber';
+  action_performed = 'Deleted a subscriber'
+ORDER BY
+  user_id;
 
 -- pkt. 3 -> widok wyświetlający nazwę użytkowników oraz datę ich edycji
 CREATE VIEW user_edit_view AS
 SELECT
+  user_id,
   subscriber_name,
   date_added AS edit_date
 FROM
   audit_subscribers
 WHERE
-  action_performed = 'Updated a subscriber';
+  action_performed = 'Updated a subscriber'
+ORDER BY
+  user_id, edit_date;
 
 -- pkt. 4 -> widok wyświetlający nazwę już usuniętych użytkowników oraz daty ich dodania i usunięcia
 CREATE VIEW deleted_users_history_view AS
@@ -133,4 +161,6 @@ WHERE
     SELECT user_id
     FROM audit_subscribers
     WHERE action_performed = 'Deleted a subscriber'
-  );
+  )
+ORDER BY
+  user_id;
---
+++ zad3/views.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+
+<!-- Autor: Marek Górski -->
+<!-- nr indeksu: 155647 -->
+<!-- grupa D1 -->
+<!-- rok akademicki 2024/2025 -->
+<!-- semestr V -->
+
+    <head>
+        <title>Internetowe Bazy Danych</title>
+        <style>
+            table, th, td {
+                border: 1px solid black;
+                border-collapse: collapse;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Widoki z zadania trzeciego</h1>
+            <?php
+                $host = 'localhost';
+                $dbname = 'test';
+                $username = 'int_baz';
+                $password = '1nt3rn3t0w3_b4zy';
+                $port = 3306;
+
+                try {
+                    $pdo = new PDO('mysql:host=' . $host . ';dbname=' . $dbname . ';port=' . $port, $username, $password);
+
+                    $sql = 'SELECT * FROM `user_creation_view`';
+                    $stmt = $pdo->query($sql);
+                    echo '<h4>ad.1. Widok wyświetlający nazwę użytkowników oraz datę ich dodania</h4>';
+                    echo '<table style="width:1000px">';
+                    echo '<tr><th>ID</th><th>Name</th><th>Creation date</th></tr>';
+                    foreach($stmt as $row) {
+                        echo '<tr>';
+                        echo '<td>'. htmlspecialchars($row['user_id']) .'</td>';
+                        echo '<td>'. htmlspecialchars($row['subscriber_name']) .'</td>';
+                        echo '<td>'. htmlspecialchars($row['creation_date']) .'</td>';
+                        echo '</tr>';
+                    }
+                    echo '</table>';                    
+
+                    $sql = 'SELECT * FROM `user_deletion_view`';
+                    $stmt = $pdo->query($sql);
+                    echo '<h4>ad.2. Widok wyświetlający nazwę użytkowników oraz datę ich usunięcia</h4>';
+                    echo '<table style="width:1000px">';
+                    echo '<tr><th>ID</th><th>Name</th><th>Deletion date</th></tr>';
+                    foreach($stmt as $row) {
+                        echo '<tr>';
+                        echo '<td>'. htmlspecialchars($row['user_id']) .'</td>';
+                        echo '<td>'. htmlspecialchars($row['subscriber_name']) .'</td>';
+                        echo '<td>'. htmlspecialchars($row['deletion_date']) .'</td>';
+                        echo '</tr>';
+                    }
+                    echo '</table>';                    
+                    
+                    $sql = 'SELECT * FROM `user_edit_view`';
+                    $stmt = $pdo->query($sql);
+                    echo '<h4>ad.3. Widok wyświetlający nazwę użytkowników oraz datę ich edycji</h4>';
+                    echo '<table style="width:1000px">';
+                    echo '<tr><th>ID</th><th>Name</th><th>Edit date</th></tr>';
+                    foreach($stmt as $row) {
+                        echo '<tr>';
+                        echo '<td>'. htmlspecialchars($row['user_id']) .'</td>';
+                        echo '<td>'. htmlspecialchars($row['subscriber_name']) .'</td>';
+                        echo '<td>'. htmlspecialchars($row['edit_date']) .'</td>';
+                        echo '</tr>';
+                    }
+                    echo '</table>';
+
+                    $sql = 'SELECT * FROM `deleted_users_history_view`';
+                    $stmt = $pdo->query($sql);
+                    echo '<h4>ad.4. Widok wykonanych akcji na już usuniętych użytkownikach</h4>';
+                    echo '<table style="width:1000px">';
+                    echo '<tr><th>ID</th><th>Name</th><th>Performed action</th><th>Action date</th></tr>';
+                    foreach($stmt as $row) {
+                        echo '<tr>';
+                        echo '<td>'. htmlspecialchars($row['user_id']) .'</td>';
+                        echo '<td>'. htmlspecialchars($row['subscriber_name']) .'</td>';
+                        echo '<td>'. htmlspecialchars($row['action_performed']) .'</td>';
+                        echo '<td>'. htmlspecialchars($row['action_date']) .'</td>';
+                        echo '</tr>';
+                    }
+                    echo '</table>';
+
+                    $sql = 'SELECT * FROM `existing_users_view`';
+                    $stmt = $pdo->query($sql);
+                    echo '<h4>ad.5. Widok wyświetlający tylko istniejących użytkowników (bez korzystania z tabelki subscribers)</h4>';
+                    echo '<table style="width:1000px">';
+                    echo '<tr><th>ID</th><th>Name</th></tr>';
+                    foreach($stmt as $row) {
+                        echo '<tr>';
+                        echo '<td>'. htmlspecialchars($row['user_id']) .'</td>';
+                        echo '<td>'. htmlspecialchars($row['subscriber_name']) .'</td>';
+                        echo '</tr>';
+                    }
+                    echo '</table>';
+                } catch (PDOException $e) {
+                    echo "Wystąpił błąd";
+                    $errormsg= "[" . date('Y-m-d H:i:s') . "] " . (string)$e . PHP_EOL;
+                    error_log($errormsg, 3, 'error_log.log');
+                }
+            ?>
+        <p><a href="index.php">Dodaj użytkownika</a></p>
+    </body>
+</html>
